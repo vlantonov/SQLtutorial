a44d8c290ba7aed8dfb1212070d38aac197a889b
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-12-28 17:31:00 +0200

    Add BETWEEN example

---
+++ 05_between/5_between_a.sql
@@ -0,0 +1,4 @@
+SELECT * 
+FROM customers
+-- WHERE points >= 1000 AND points <= 3000;
+WHERE points BETWEEN 1000 AND 3000;
---
+++ 05_between/5_between_b.sql
@@ -0,0 +1,3 @@
+SELECT * 
+FROM customers
+WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01';
